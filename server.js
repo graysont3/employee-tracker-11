b909893573d90b702d830d12f020951c3024d58e
Star Child <tajagrayson@Tajas-MacBook-Air.local>
2023-07-18 18:31:47 -0400

    set up schema

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const console = require("console");
 const { default: inquirer } = require("inquirer");
+const { allowedNodeEnvironmentFlags } = require("process");
 
 
 const db = mysql.createConnection(
@@ -31,5 +32,39 @@ function start () {
                 'Quit'
             ]
         })
+        .then((response) => {
+            switch (response.action) {
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
 
-}
+                case "View all roles":
+                    viewAllRoles();
+                    break;
+
+                case "View all employees":
+                    viewAllEmployees();
+                    break;
+
+                case "Add a department":
+                    addDepartment();
+                    break;
+
+                case "Add a role":
+                    addRole();
+                    break;
+
+                case "Add an employee":
+                    addEmployee();
+                    break;
+
+                case "Update an employee role":
+                    updateEmployeeRole();
+                    break;
+
+                case 'Quit':
+                    quitDB();
+                    break;    
+            }
+        })
+};
